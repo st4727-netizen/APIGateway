c2d21cfb2d4972e8080ea71583694271363d06e0
st4727-netizen <st4727@utr.edu.mx>
2026-07-01 08:13:56 -0600

    conexion a mongo

--- index.js
+++ index.js
@@ -1,10 +1,30 @@
-const express = require('express');
+const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const connectDB = require("./src/config/database");
+
+// Importar los modelos
+require("./src/modules/schemas/user.schema");
+require("./src/modules/schemas/product.schema");
+require("./src/modules/schemas/cart.schema");
+require("./src/modules/schemas/order.schema");
+require("./src/modules/schemas/payment.schema");
 
 const app = express();
-const PORT = 5100;
 
-console.log('Hello World');
+app.use(express.json());
+
+const PORT = process.env.PORT || 5100;
+
+// Conectar a MongoDB
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("Cleaning Store API Running");
+});
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`🚀 Server running on port ${PORT}`);
 });
